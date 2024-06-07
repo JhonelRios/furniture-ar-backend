b13d69755298097bfe7386ee86753390b6aed9d1
jhonelrios <riosjaimes.j@gmail.com>
2024-06-07 01:12:53 -0500

    test: mock classe

--- src/test/java/com/forniturear/forniturear/ForniturearApplicationTests.java
+++ src/test/java/com/forniturear/forniturear/ForniturearApplicationTests.java
@@ -1,13 +1,40 @@
 package com.forniturear.forniturear;
 
+import com.forniturear.forniturear.controllers.FurnitureController;
+import com.forniturear.forniturear.repositories.IFurnitureRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class ForniturearApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	private MockMvc mockMvc;
 
+	@Mock
+	private IFurnitureRepository furnitureRepository;
+
+	@InjectMocks
+	private FurnitureController furnitureController;
+
+	@BeforeEach
+	void setup(WebApplicationContext wac) {
+		this.mockMvc = MockMvcBuilders.standaloneSetup(furnitureController).build();
+	}
 }
